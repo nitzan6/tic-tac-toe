c9ee2aebf591c088bae0899e5785bc7ff06b9892
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-28 16:39:53 +0300

    Add Grid script

---
+++ Assets/Scripts/UI/InteractionHandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TicTacToe.UI
+{
+    public class InteractionHandler : MonoBehaviour
+    {
+        public void HandleStuff()
+        {
+            Debug.Log("TEST");
+        }
+    }
+}
+
