7b0a89b5158bb8eeb3ac3bc07d99d58fea07391c
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-28 18:01:47 +0300

    Create GameProgression class

---
+++ Assets/Scripts/GameProgression/GameProgressionHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace TicTacToe.GameProgression
+{
+    public class GameProgressionHandler : MonoBehaviour
+    {
+
+        public void HandleGameStart()
+        {
+
+        }
+    }
+}
+
