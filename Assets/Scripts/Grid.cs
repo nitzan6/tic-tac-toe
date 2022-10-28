1401e57aa5e00806a1579c1988626d87f882bc73
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-28 16:40:07 +0300

    Add grid script

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grid
+{
+    public class Grid : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+    
+        void Update()
+        {
+            
+        }
+    }
+}
+
