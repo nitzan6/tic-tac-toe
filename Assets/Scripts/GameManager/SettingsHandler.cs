40624ce1875961045e28cc29d95365d873589d61
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-28 16:39:30 +0300

    Create SettingsHandler script

---
+++ Assets/Scripts/GameManager/SettingsHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace TicTacToe.GameManagement
+{
+    public class SettingsHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameSettings _gameSettings;
+
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+            
+        }
+    }
+}
