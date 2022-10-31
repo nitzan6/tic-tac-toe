3426e379cee3e9f0f031fd84345aefd6bb53c335
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-31 16:55:11 +0200

    Add documentation in PlayerFactory class

--- Assets/Scripts/Gamemode/PlayerFactory.cs
+++ Assets/Scripts/Gamemode/PlayerFactory.cs
@@ -8,11 +8,13 @@ namespace TicTacToe.GameManagment.Setup
 {
     public static class PlayerFactory
     {
+        //Here I am getting all types which implement the IPlayer interface using reflection
+        //Excluding the interface itself
         private static Type[] _playerTypes = Assembly.GetAssembly(typeof(IPlayer)).GetTypes()
                 .Where(type => type.IsClass && typeof(IPlayer).IsAssignableFrom(type))
                 .ToArray();
 
-        public static Type[] GetPlayersFromGamemode(Gamemode gamemode)
+        public static Type[] GetPlayerTypesFromGamemode(Gamemode gamemode)
         {
             Type[] gamemodePlayerTypes = new Type[2];
 
@@ -30,22 +32,6 @@ namespace TicTacToe.GameManagment.Setup
             }
 
             return gamemodePlayerTypes;
-            
-            //playersTypes[0] = (gamemode.Player1 as LocalPlayer) || (gamemode.Player1 as AI);
-
-            /*
-            switch (gamemode)
-            {
-                case (GameMode.LocalVsAI):
-                    playersTypes = new Type[] { typeof(LocalPlayer), typeof(AI) };
-                    break;
-                case (GameMode.LocalVsLocal):
-                    playersTypes = new Type[] { typeof(LocalPlayer), typeof(LocalPlayer) };
-                    break;
-                default:
-                    throw new Exception($"The gamemode {gamemode} is not supported.");
-            }
-            */
         }
     }
 }
