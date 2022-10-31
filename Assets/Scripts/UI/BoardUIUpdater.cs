83c503914c11f4f0966a819c34f6af8709f6cce1
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-31 16:56:22 +0200

    Refactor BoardUIUpdater

--- Assets/Scripts/UI/BoardUIUpdater.cs
+++ Assets/Scripts/UI/BoardUIUpdater.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TicTacToe.GameManagement;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,6 +23,7 @@ namespace TicTacToe.UI
         {
             GameEvents.Instance.onMadeMove += UpdateBoardUI;
             GameEvents.Instance.onGameStart += ClearBoardUI;
+            GameEvents.Instance.onUndoLastMove += ClearCell;
         }
 
         private void UpdateBoardUI(Vector2Int position, Symbol symbol)
@@ -33,25 +35,47 @@ namespace TicTacToe.UI
             }
 
             LoadSprite(position, _oSprite);
-
         }
 
         private void LoadSprite(Vector2Int position, Sprite sprite)
         {
-            //Convert the 2D vector we use on the board to an index in the array we got
-            int index = position.y * Consts.BOARD_WIDTH + position.x;
+            int index = GetArrayIndexFromPosition(position);
+            PlaceSprite(_cellsImageComponents[index], sprite);
+        }
 
-            _cellsImageComponents[index].sprite = sprite;
-            _cellsImageComponents[index].color = Color.white;
+        //Convert the 2D vector we use on the board to an index in the image components array
+        private int GetArrayIndexFromPosition(Vector2Int position)
+        {
+            return position.y * Consts.BOARD_WIDTH + position.x;
         }
 
         private void ClearBoardUI()
         {
             foreach (Image imageComponent in _cellsImageComponents)
             {
-                imageComponent.sprite = null;
-                imageComponent.color = new Color(1, 1, 1 ,0);
+                ClearSprite(imageComponent);
             }
+        }
+
+        private void ClearCell(List<Vector2Int> positions)
+        {
+            foreach (Vector2Int position in positions)
+            {
+                int arrayIndex = GetArrayIndexFromPosition(position);
+                ClearSprite(_cellsImageComponents[arrayIndex]);
+            }
+        }
+
+        private void PlaceSprite(Image image, Sprite sprite)
+        {
+            image.sprite = sprite;
+            image.color = Color.white;
+        }
+
+        private void ClearSprite(Image image)
+        {
+            image.sprite = null;
+            image.color = new Color(1, 1, 1, 0);
         }
 
         void OnDisable()
