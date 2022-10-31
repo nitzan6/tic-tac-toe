075363a2a52ca5defabfe728e43b47c209dbf3b2
nitzanpeleg <nitzanpel214@gmail.com>
2022-11-01 00:30:44 +0200

    Remove nullable Symbol type from CheckWinner function

--- Assets/Scripts/GameProgression/Board/Referee.cs
+++ Assets/Scripts/GameProgression/Board/Referee.cs
@@ -2,12 +2,13 @@ namespace TicTacToe.GameProgression
 {
     public class Referee
     {
-        public Symbol? CheckWinner(Symbol[,] boardCells)
+        //Return the winning symbol, return empty if there is no winner
+        public enSymbol GetWinner(enSymbol[,] boardCells)
         {
             //vertical winner check
             for (int i = 0; i < boardCells.GetLength(0); i++)
             {
-                if (boardCells[i,0] != Symbol.EMPTY && boardCells[i, 0] == boardCells[i, 1] && boardCells[i,0] == boardCells[i,2])
+                if (boardCells[i,0] != enSymbol.EMPTY && boardCells[i, 0] == boardCells[i, 1] && boardCells[i,0] == boardCells[i,2])
                 {
                     return boardCells[i, 0];
                 }
@@ -16,33 +17,33 @@ namespace TicTacToe.GameProgression
             //horizontal winner check
             for (int i = 0; i < boardCells.GetLength(1); i++)
             {
-                if (boardCells[0, i] != Symbol.EMPTY && boardCells[0, i] == boardCells[1, i] && boardCells[0, i] == boardCells[2, i])
+                if (boardCells[0, i] != enSymbol.EMPTY && boardCells[0, i] == boardCells[1, i] && boardCells[0, i] == boardCells[2, i])
                 {
                     return boardCells[0, i];
                 }
             }
 
             //diagonals winner check
-            if (boardCells[0, 0] != Symbol.EMPTY && boardCells[0, 0] == boardCells[1, 1] && boardCells[0, 0] == boardCells[2, 2])
+            if (boardCells[0, 0] != enSymbol.EMPTY && boardCells[0, 0] == boardCells[1, 1] && boardCells[0, 0] == boardCells[2, 2])
             {
                 return boardCells[0, 0];
             }
             
-            if (boardCells[2, 0] != Symbol.EMPTY && boardCells[2, 0] == boardCells[1, 1] && boardCells[2, 0] == boardCells[0, 2])
+            if (boardCells[2, 0] != enSymbol.EMPTY && boardCells[2, 0] == boardCells[1, 1] && boardCells[2, 0] == boardCells[0, 2])
             {
                 return boardCells[2, 0];
             }
 
-            return null;
+            return enSymbol.EMPTY;
         }
 
-        public bool IsDraw(Symbol[,] boardCells)
+        public bool CheckDraw(enSymbol[,] boardCells)
         {
             for (int i = 0; i < boardCells.GetLength(0); i++)
             {
                 for (int j = 0; j < boardCells.GetLength(1); j++)
                 {
-                    if (boardCells[i, j] == Symbol.EMPTY)
+                    if (boardCells[i, j] == enSymbol.EMPTY)
                     {
                         return false;
                     }
