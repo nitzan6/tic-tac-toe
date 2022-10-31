94cf9f45fa2deb031050a5d8fb831a0089716fec
nitzanpeleg <nitzanpel214@gmail.com>
2022-10-31 18:17:53 +0200

    Fix players being still able to undo moves after game is over

--- Assets/Scripts/GameManager/GameSetup/UndoManager.cs
+++ Assets/Scripts/GameManager/GameSetup/UndoManager.cs
@@ -1,3 +1,4 @@
+using TicTacToe.GameManagement;
 using TicTacToe.GameManagement.Gamemodes;
 using UnityEngine;
 
@@ -7,9 +8,30 @@ public class UndoManager : MonoBehaviour
     private GameObject _undoButton;
     public bool CanUndo { get; private set; }
 
+    void OnEnable()
+    {
+        GameEvents.Instance.onGameStart += EnableUndoAbility;
+        GameEvents.Instance.onGameEnded += DisableUndoAbility;
+    }
+    private void EnableUndoAbility()
+    {
+        CanUndo = true;
+    }
+
+    private void DisableUndoAbility(GameState _)
+    {
+        CanUndo = false;
+    }
+
     public void ApplyUndoSettings(Gamemode gamemode)
     {
         CanUndo = gamemode.IsGameModeVsComputer();
         _undoButton.SetActive(CanUndo);
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.Instance.onGameStart -= EnableUndoAbility;
+        GameEvents.Instance.onGameEnded -= DisableUndoAbility;
     }
 }
