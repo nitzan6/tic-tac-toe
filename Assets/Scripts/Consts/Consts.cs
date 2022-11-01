8b0e63eb46d82cdff8f5df2ce46bb03e95bf8b80
nitzanpeleg <nitzanpel214@gmail.com>
2022-11-01 18:50:20 +0200

    Remove unnecessary consts

--- Assets/Scripts/Consts/Consts.cs
+++ Assets/Scripts/Consts/Consts.cs
@@ -6,8 +6,4 @@ public static class Consts
 
     public const string PLAYER_1_NAME = "Player One";
     public const string PLAYER_2_NAME = "Player Two";
-    public const string COMPUTER_NAME = "Computer";
-
-    public const string GAME_END_MESSAGE_COMPONENT = "Message";
-    public const string WINNER_IMAGE = "WinnerImage";
 }
